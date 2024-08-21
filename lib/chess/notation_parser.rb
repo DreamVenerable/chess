8e2eea306dd659fc7ec189fd2c161fd862f03ce2
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2024-08-21 13:43:21 +0200

    simplified notation requirement

--- lib/chess/notation_parser.rb
+++ lib/chess/notation_parser.rb
@@ -1,10 +1,9 @@
 module Chess
   module NotationParser
     def self.parse_move(move)
-      # Ex. ng1f3
-      # piece = move[0] # n = Knight
-      start_pos = convert_to_index(move[1..2]) # g1 = where the piece is located
-      dest_pos = convert_to_index(move[3..4]) # f3 = destination of piece
+      # Ex. g1f3
+      start_pos = convert_to_index(move[0..1]) # g1 = where the piece is located
+      dest_pos = convert_to_index(move[2..3]) # f3 = destination of piece
 
       [start_pos, dest_pos]
     end
